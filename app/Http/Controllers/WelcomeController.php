f323840e0f7ea5d3aa3c0c909f174a274cb3a900
Hitalo-Lima <hitalolima173@gmail.com>
2024-10-09 09:39:43 -0300

    aula 01

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -2,12 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class WelcomeController extends Controller
 {
     public function __invoke()
     {
+        // $user = User::query()->create([
+        //     'name' => 'José Braga',
+        //     'email' => 'josebraga@gmail.com',
+        //     'password' => 'jbraga'
+        // ]);
+
+        // $user = User::find(1);
+
+        // Auth::login($user);
+        // dd($user);
+
+
         return view('welcome');
     }
 }
